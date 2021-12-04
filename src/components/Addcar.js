4720ea774e5894ccc2b004535cdb7e539d535abf
Lauri <hakulinen.lauri@gmail.com>
2021-12-04 12:46:40 +0000

    Delete changes

---
+++ src/components/Addcar.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { Button, TextField, Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
+
+export default function Addcar() {
+    const [open, setOpen] = React.useState(false);
+    const [car, setCar] = React.useState({
+        brand: '', model: '', color: '', fuel: '', year: '', price: ''
+    })
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const handleInputChange = (event) => {
+        setCar({...car, [event.target.name]: event.target.value})
+    }
+
+    return (
+        <div>
+            <Button style={{margin: 10}} variant="outlined" color="primary" onClick={handleClickOpen}>Add Car</Button>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">New Car</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        name="brand"
+                        value={car.brand}
+                        onChange={e => handleInputChange(e)}
+                        label="Brand"
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        name="model"
+                        value={car.model}
+                        onChange={e => handleInputChange(e)}
+                        label="Model"
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        name="color"
+                        value={car.color}
+                        onChange={e => handleInputChange(e)}
+                        label="Color"
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        name="fuel"
+                        value={car.fuel}
+                        onChange={e => handleInputChange(e)}
+                        label="Fuel"
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        name="year"
+                        value={car.year}
+                        onChange={e => handleInputChange(e)}
+                        label="Year"
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        name="price"
+                        value={car.price}
+                        onChange={e => handleInputChange(e)}
+                        label="Price"
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">Cancel</Button>
+                    <Button onClick={handleClose} color="primary">Save</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    )
+}
--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import { Button, Stack } from '@mui/material';
+import Addcar from './Addcar';
 
 export default function Carlist() {
     const [cars, setCars] = useState([]);
@@ -16,23 +18,31 @@ export default function Carlist() {
         .then(data => setCars(data._embedded.cars))
     }
 
-    const deleteCar = () => {
-        const rows = gridRef.current.getSelectedRows()
-        gridRef.current.applyTransaction({ remove: rows })
+    const deleteCar = (url) => {
+        fetch(url, {method: 'Delete'})
+        .then(result => fetchData())
+        .catch(error => console.error(error))
     }
 
     const columns = [
-        {headerName: "Brand", field:"brand", checkboxSelection: true, sortable: true, filter: true},
+        {headerName: "Brand", field:"brand", sortable: true, filter: true},
         {headerName: "Model", field:"model", sortable: true, filter: true},
         {headerName: "Color", field:"color", sortable: true, filter: true},
         {headerName: "Fuel", field:"fuel", sortable: true, filter: true},
         {headerName: "Year", field:"year", sortable: true, filter: true},
         {headerName: "Price", field:"price", sortable: true, filter: true},
+        {headerName: "Delete", field: "_links.self.href", sortable: false, filter: false, 
+        cellRendererFramework: params => {
+            const url = params.value;
+            return (<Button size="small" variant="contained" color="error" onClick={() => deleteCar(url)}>Delete</Button>)
+        }}
     ]
 
     return (
         <div className="ag-theme-material"style={{height:'700px',width:'100%',margin:'auto'}}>
-            <button onClick={deleteCar}>Delete</button>
+            <Stack direction="row">
+                <Addcar />
+            </Stack>
             <AgGridReact
                 ref={gridRef}
                 onGridReady={ params => gridRef.current = params.api }
